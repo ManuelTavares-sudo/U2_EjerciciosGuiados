f8bf36c21f09d3e029798312ffec95347945b3db
ManuelTavares-sudo <1224100719.math@gmail.com>
2025-10-20 09:19:21 -0600

    Archivos
    Archivos de los ejercicios

---
+++ Ejercicios Guiados/Ejercicio 3/IStack.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Stacks;
+
+/**
+ *
+ * @author tavar
+ */
+public interface IStack<T> {
+    void push(T element);
+    T pop();
+    T peek();
+    boolean isEmpty();
+}
